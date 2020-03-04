99152884906c80924645784b9a837befe0682a58
Conner <connerfissell91@gmail.com>
2020-03-03 17:16:12 -0800

    added RNG

--- RNG.cpp
+++ RNG.cpp
@@ -32,23 +32,47 @@ int main()
     int ARRAY_SIZE1 = 1000;
     int ARRAY_SIZE2 = 10000;    
     int ARRAY_SIZE3 = 100000;
-
+    int range = 1000000;
+    int num1[ARRAY_SIZE1];
+    int num2[ARRAY_SIZE2];
+    int num3[ARRAY_SIZE3];
+    string textFile1 = "1000_Integers";
+    string textFile2 = "10000_Integers";
+    string textFile3 = "100000_Integers";
+    ofstream outFile1, outFile2, outFile3;
 
 	//RNG
 	unsigned seed = time(0);
 	srand(seed);
 
-    cout << seed << endl;
+    // Create files to store numbers
+    outFile1.open(textFile1);
+    outFile2.open(textFile2);
+    outFile3.open(textFile3);
 
-    int num = rand() % 106;
-    cout << num << endl;
+    // Fill up the files
+    for (int i = 0; i < ARRAY_SIZE1; i++)
+    {
+        num1[i] = rand() % range;
+        outFile1 << num1[i];
+    }
+
+    for (int j = 0; j < ARRAY_SIZE2; j++)
+    {
+        num2[j] = rand() % range;
+        outFile2 << num2[j];
+    }
+
+    for (int k = 0; k < ARRAY_SIZE3; k++)
+    {
+        num3[k] = rand() % range;
+        outFile3 << num3[k];
+    }
 
 
-
-
-
-
-
+    outFile1.close();
+    outFile2.close();
+    outFile3.close();
 
 	
 	cout << endl;
@@ -56,6 +80,7 @@ int main()
 	cout << __DATE__ << "  " __TIME__ << endl;
 	cout << endl;
 
+    system("pause");
+
 	return 0;
 }
-
